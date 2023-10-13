b2f43092949b78b57ae4a73c1057d736b0aa937b
Daniel Ramirez Gallo <daniel.ramirez@grupokonecta.com>
2023-10-13 17:49:42 -0500

    test: should subtract two float numbers

--- __test__/actions/subtraction.test.js
+++ __test__/actions/subtraction.test.js
@@ -12,4 +12,16 @@ describe("Subtraction action", () => {
     // Assert
     expect(result).toBe(-1);
   });
+
+  it("should subtract two float numbers", () => {
+    // Arrange
+    const number1 = 1.5;
+    const number2 = 2.5;
+
+    // Act
+    const result = subtraction(number1, number2);
+
+    // Assert
+    expect(result).toBe(-1);
+  });
 });
