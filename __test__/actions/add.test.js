39b8e39c09de51c42f2f23b76d85c6f43cd98001
Daniel Ramirez Gallo <daniel.ramirez@grupokonecta.com>
2023-10-13 17:42:57 -0500

    test: add float numbers

--- __test__/actions/add.test.js
+++ __test__/actions/add.test.js
@@ -2,6 +2,50 @@ const add = require("@/actions/add");
 
 describe("Add action", () => {
   it("should add two positive numbers", () => {
-    expect(add(1, 2)).toBe(3);
+    // Arrange
+    const number1 = 1;
+    const number2 = 2;
+
+    // Act
+    const result = add(number1, number2);
+
+    // Assert
+    expect(result).toBe(3);
+  });
+
+  it("should add two negative numbers", () => {
+    // Arrange
+    const number1 = -10;
+    const number2 = -5;
+
+    // Act
+    const result = add(number1, number2);
+
+    // Assert
+    expect(result).toBe(-15);
+  });
+
+  it("should add a positive and a negative number", () => {
+    // Arrange
+    const number1 = 10;
+    const number2 = -5;
+
+    // Act
+    const result = add(number1, number2);
+
+    // Assert
+    expect(result).toBe(5);
+  });
+
+  it("should add float numbers", () => {
+    // Arrange
+    const number1 = 1.3333333;
+    const number2 = 2.5;
+
+    // Act
+    const result = add(number1, number2);
+
+    // Assert
+    expect(result).toBe(3.8333333);
   });
 });
