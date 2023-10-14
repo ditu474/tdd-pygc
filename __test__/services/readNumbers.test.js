b02830d7e87bdfa34311338be497db5f53d39648
Daniel Ramirez Gallo <daniel.ramirez@grupokonecta.com>
2023-10-13 20:18:26 -0500

    test: read numbers from console

--- __test__/services/readNumbers.test.js
+++ __test__/services/readNumbers.test.js
@@ -4,13 +4,17 @@ const readFromConsole = require("@/services/readFromConsole");
 jest.mock("@/services/readFromConsole");
 
 describe("Read numbers from console service", () => {
-  it("should read two numbers from console", async () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should read two int numbers from console", async () => {
     // Arrange
-    const firstNumber = "123";
-    const secondNumber = "456";
+    const firstNumber = 123;
+    const secondNumber = 456;
     readFromConsole
-      .mockResolvedValueOnce(firstNumber)
-      .mockResolvedValueOnce(secondNumber);
+      .mockResolvedValueOnce(firstNumber.toString())
+      .mockResolvedValueOnce(secondNumber.toString());
 
     // Act
     const result = await readNumbers();
@@ -19,6 +23,68 @@ describe("Read numbers from console service", () => {
     expect(readFromConsole).toHaveBeenCalledTimes(2);
     expect(readFromConsole).toHaveBeenCalledWith("Enter first number: ");
     expect(readFromConsole).toHaveBeenCalledWith("Enter second number: ");
+    expect(result).toEqual([firstNumber, secondNumber]);
+  });
+
+  it("Should read two float numbers from console", async () => {
+    // Arrange
+    const firstNumber = 123.456;
+    const secondNumber = 456.789;
+    readFromConsole
+      .mockResolvedValueOnce(firstNumber.toString())
+      .mockResolvedValueOnce(secondNumber.toString());
+
+    // Act
+    const result = await readNumbers();
+
+    // Assert
+    expect(readFromConsole).toHaveBeenCalledTimes(2);
+    expect(readFromConsole).toHaveBeenCalledWith("Enter first number: ");
+    expect(readFromConsole).toHaveBeenCalledWith("Enter second number: ");
+    expect(result).toEqual([firstNumber, secondNumber]);
+  });
+
+  it("Should read two negative numbers from console", async () => {
+    // Arrange
+    const firstNumber = -123.123;
+    const secondNumber = -456;
+    readFromConsole
+      .mockResolvedValueOnce(firstNumber.toString())
+      .mockResolvedValueOnce(secondNumber.toString());
+
+    // Act
+    const result = await readNumbers();
+
+    // Assert
+    expect(readFromConsole).toHaveBeenCalledTimes(2);
+    expect(readFromConsole).toHaveBeenCalledWith("Enter first number: ");
+    expect(readFromConsole).toHaveBeenCalledWith("Enter second number: ");
+    expect(result).toEqual([firstNumber, secondNumber]);
+  });
+
+  it("Should ask again for a number if the input is not a number", async () => {
+    // Arrange
+    const nanInput = "not a number";
+    const firstNumber = 123;
+    const secondNumber = 456;
+    readFromConsole
+      .mockResolvedValueOnce(nanInput)
+      .mockResolvedValueOnce(nanInput)
+      .mockResolvedValueOnce(firstNumber)
+      .mockResolvedValueOnce(nanInput)
+      .mockResolvedValueOnce(secondNumber.toString());
+
+    // Act
+    const result = await readNumbers();
+
+    // Assert
+    const nanMessage = "The input is not a number. Please enter a number: ";
+    expect(readFromConsole).toHaveBeenCalledTimes(5);
+    expect(readFromConsole).toHaveBeenNthCalledWith(1, "Enter first number: ");
+    expect(readFromConsole).toHaveBeenNthCalledWith(2, nanMessage);
+    expect(readFromConsole).toHaveBeenNthCalledWith(3, nanMessage);
+    expect(readFromConsole).toHaveBeenNthCalledWith(4, "Enter second number: ");
+    expect(readFromConsole).toHaveBeenNthCalledWith(5, nanMessage);
     expect(result).toEqual([firstNumber, secondNumber]);
   });
 });
