b51022c426d8b23f281dd66709e22b284087ace3
Daniel Ramirez Gallo <daniel.ramirez@grupokonecta.com>
2023-10-13 20:18:16 -0500

    feat: read numbers from console

--- src/services/readNumbers.js
+++ src/services/readNumbers.js
@@ -1,10 +1,39 @@
 const readFromConsole = require("@/services/readFromConsole");
 
-const readNumbers = async () => {
-  const firstNumber = await readFromConsole("Enter first number: ");
-  const secondNumber = await readFromConsole("Enter second number: ");
+const convertToNumber = (value) => {
+  try {
+    const number = parseFloat(value);
+    return number;
+  } catch (_) {
+    return NaN;
+  }
+};
 
-  return [firstNumber, secondNumber];
+const readNumbers = async () => {
+  let isNumberFirst = false;
+  let isNumberSecond = false;
+
+  let firstNumber = await readFromConsole("Enter first number: ");
+  isNumberFirst = !isNaN(convertToNumber(firstNumber));
+
+  while (!isNumberFirst) {
+    firstNumber = await readFromConsole(
+      "The input is not a number. Please enter a number: ",
+    );
+    isNumberFirst = !isNaN(convertToNumber(firstNumber));
+  }
+
+  let secondNumber = await readFromConsole("Enter second number: ");
+  isNumberSecond = !isNaN(convertToNumber(secondNumber));
+
+  while (!isNumberSecond) {
+    secondNumber = await readFromConsole(
+      "The input is not a number. Please enter a number: ",
+    );
+    isNumberSecond = !isNaN(convertToNumber(secondNumber));
+  }
+
+  return [convertToNumber(firstNumber), convertToNumber(secondNumber)];
 };
 
 module.exports = readNumbers;
